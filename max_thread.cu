ff1b710c627eff7a85f7c19e8754379e8e052568
Mohammad Saqib <m.saqib@student.uw.edu.pl>
2023-04-02 21:13:57 +0200

    max_thread.cu added

--- max_thread.cu
+++ max_thread.cu
@@ -1,23 +1,21 @@
 #include <stdio.h>
 #include <cuda_runtime.h>
 
-int main() {
+int main() 
+{
     int deviceCount;
     cudaGetDeviceCount(&deviceCount);
-    for (int i = 0; i < deviceCount; i++) {
+    for (int i = 0; i < deviceCount; i++) 
+	{
         cudaDeviceProp deviceProp;
         cudaGetDeviceProperties(&deviceProp, i);
         printf("Device %d: %s\n", i, deviceProp.name);
         printf("  Maximum threads per block: %d\n", deviceProp.maxThreadsPerBlock);
     }
-	
-	
-	threads_per_block = dim3(16, 16, 16);
-	blocks_per_grid = dim3((length + threads_per_block.x - 1) / threads_per_block.x,
-						(length + threads_per_block.y - 1) / threads_per_block.y,
-						(length + threads_per_block.z - 1) / threads_per_block.z);	
-	
+	dim3 threads_per_block(16, 16, 16);
+	dim3 blocks_per_grid((length + threads_per_block.x - 1) / threads_per_block.x,
+						 (length + threads_per_block.y - 1) / threads_per_block.y,
+						 (length + threads_per_block.z - 1) / threads_per_block.z);	
 	printf("blocks_per_grid dimensions: (%d, %d, %d)\n", blocks_per_grid.x, blocks_per_grid.y, blocks_per_grid.z);
-	
     return 0;
 }
