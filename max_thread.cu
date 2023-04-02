b7054dd0ff3939fbe695480b358570bd30ce6e3b
Mohammad Saqib <m.saqib@student.uw.edu.pl>
2023-04-02 21:10:06 +0200

    max_thread.cu added

--- max_thread.cu
+++ max_thread.cu
@@ -10,5 +10,14 @@ int main() {
         printf("Device %d: %s\n", i, deviceProp.name);
         printf("  Maximum threads per block: %d\n", deviceProp.maxThreadsPerBlock);
     }
+	
+	
+	threads_per_block = dim3(16, 16, 16);
+	blocks_per_grid = dim3((length + threads_per_block.x - 1) / threads_per_block.x,
+						(length + threads_per_block.y - 1) / threads_per_block.y,
+						(length + threads_per_block.z - 1) / threads_per_block.z);	
+	
+	printf("blocks_per_grid dimensions: (%d, %d, %d)\n", blocks_per_grid.x, blocks_per_grid.y, blocks_per_grid.z);
+	
     return 0;
 }
