af2919e22cbe52d1afa0825667652d8177e625ff
Mohammad Saqib <m.saqib@student.uw.edu.pl>
2023-04-09 22:10:22 +0200

    committed

--- matrix_add_2d.cu
+++ matrix_add_2d.cu
@@ -1,3 +1,5 @@
+////    test.cu //////////
+
 #include "common.hpp"
 
 const int min_ = 0;
@@ -6,19 +8,19 @@ const int max_ = 10;
 
 __global__ void AddMatrixKernel(t *A, t *B, t *C, int N) 
 {
-	int dimx = N;
-	int dimy = N;
-	int dimz = N;
+    int dimx = N;
+    int dimy = N;
+    int dimz = N;
 
     int i = blockIdx.x * blockDim.x + threadIdx.x;
     int j = blockIdx.y * blockDim.y + threadIdx.y;
     int k = blockIdx.z * blockDim.z + threadIdx.z;
 
     if (i < N && j < N && k < N) 
-	{
+    {
         int loc_c = k * dimx * dimy + j * dimx + i;
-        int loc_a = j * dimx + i;
-        int loc_b = i * dimy + j;
+        int loc_a = k * dimx * dimy + j * dimx + i;
+        int loc_b = k * dimx * dimy + j * dimx + i;
         C[loc_c] = A[loc_a] + B[loc_b];
     }
 }
@@ -34,14 +36,14 @@ int main()
     int length;
     dim3 threads_per_block;
     dim3 blocks_per_grid;
-	
-	
-	length = 1000000;
-	host_a = (t *) malloc(sizeof(t) * length);
-	host_b = (t *) malloc(sizeof(t) * length);
-	host_c = (t *) malloc(sizeof(t) * length);
-	
-	if (host_a == nullptr || host_b == nullptr || host_c == nullptr)
+    
+    
+    length = 1000000;
+    host_a = (t *) malloc(sizeof(t) * length);
+    host_b = (t *) malloc(sizeof(t) * length);
+    host_c = (t *) malloc(sizeof(t) * length);
+    
+    if (host_a == nullptr || host_b == nullptr || host_c == nullptr)
     {
         std::cerr << "Error: Memory allocation for host arrays failed." << std::endl;
         exit(1);
@@ -51,8 +53,8 @@ int main()
     CHECK_CUDA_ERROR(cudaMalloc((void**) &device_b, sizeof(t) * length));
     CHECK_CUDA_ERROR(cudaMalloc((void**) &device_c, sizeof(t) * length));
 
-	for (int i = 0; i < length ; ++i) 
-	{
+    for (int i = 0; i < length ; ++i) 
+    {
         host_a[i] = rand_float(min_, max_);
         host_b[i] = rand_float(min_, max_);
         host_c[i] = 0;
@@ -62,15 +64,13 @@ int main()
     int max_block = 62500;
     threads_per_block = dim3(32, 8, 4); // because, 1204 = 32*8*4 
     blocks_per_grid = dim3(max_block, max_block, max_block); 
-                           // ceil((length + max_block_per_grid_per_dim-1) / 8), 
-			               // ceil((length + max_block_per_grid_per_dim-1) / 4));
 
     print_dim3("threads_per_block", threads_per_block);
     print_dim3("blocks_per_grid", blocks_per_grid);
 
     CHECK_CUDA_ERROR(cudaMemcpy(device_a, host_a, sizeof(t) * length, cudaMemcpyHostToDevice));
     CHECK_CUDA_ERROR(cudaMemcpy(device_b, host_b, sizeof(t) * length, cudaMemcpyHostToDevice));
-	
+    
     AddMatrixKernel<<<blocks_per_grid, threads_per_block>>>(device_a, device_b, device_c, 100);
 	
     CHECK_LAST_CUDA_ERROR();
