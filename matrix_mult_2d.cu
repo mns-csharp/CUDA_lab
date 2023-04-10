31d3a3d292f4aea13094d79e90b512c67852b454
Mohammad Saqib <m.saqib@student.uw.edu.pl>
2023-04-10 05:23:54 +0200

    committed

--- matrix_mult_2d.cu
+++ matrix_mult_2d.cu
@@ -4,22 +4,6 @@
 const int min_ = 0;
 const int max_ = 10;
 
-/*
-void OuterProduct(float* A, float* B, float** C, int N)
-{
-    for(int r=0 ; r<N ; r++)
-    {
-        for(int c=0 ; c<N ; c++)
-        {
-            for(int cc=0 ; cc<N ; cc++)
-            {
-                (*C)[r * N + c] += A[r * N + cc] * B[cc * N + c];
-            }
-        }
-    }
-}
-*/
-
 __global__ void MultiplyMatKernel(I* A, I* B, I* C, int N)
 {
     int dimx = N;
@@ -28,16 +12,16 @@ __global__ void MultiplyMatKernel(I* A, I* B, I* C, int N)
 
     int r = blockIdx.x * blockDim.x + threadIdx.x;
     int c = blockIdx.y * blockDim.y + threadIdx.y;
-    int d = blockIdx.z * blockDim.z + threadIdx.z;
+	int d = blockIdx.z * blockDim.z + threadIdx.z;
 
     if (r < N && c < N && d < N) 
 	{
         int loc_c = d * dimx * dimy + c * dimx + r;
- 
+		
         for (int cc=0; cc<N; cc++) 
-		{
-		    int loc_a = (cc * dimx * dimy) + (c * dimx) + r;
-		    int loc_b = (d * dimx * dimy) + (cc * dimx) + r;
+		{	
+            int loc_a = (cc * dimx * dimy) + (c * dimx) + r;
+		    int loc_b = (d * dimx * dimy) + (cc * dimx) + r;	    
             C[loc_c] += A[loc_a]*B[loc_b];
         }
     }
@@ -58,7 +42,7 @@ int main()
     dim3 blocks_per_grid;    
     
 	kernel_len = 3;
-    length = kernel_len * kernel_len * kernel_len;
+    length = kernel_len * kernel_len * 1;
     host_a = (I *) malloc(sizeof(I) * length);
     host_b = (I *) malloc(sizeof(I) * length);
     host_c = (I *) malloc(sizeof(I) * length);
@@ -75,17 +59,20 @@ int main()
 
     for (int i = 0; i < length ; ++i) 
     {
-        host_a[i] = rand_float(min_, max_);
-        host_b[i] = rand_float(min_, max_);
+        host_a[i] = i+1;
+        host_b[i] = i+1;
         host_c[i] = 0;
     }
 
-    int dimx = 2;
-    int dimy = 2;
-    int dimz = 2;
+    int dimx = kernel_len;
+    int dimy = kernel_len;
+    int dimz = 1;
 
-    dim3 threads_per_block(3, 3, 3);
-    dim3 blocks_per_grid(3, 3, 3);
+    //int max_thread = 1024;
+    threads_per_block = dim3(32, 8, 4); // because, 1204 = 32*8*4 
+    blocks_per_grid = dim3((dimx + threads_per_block.x - 1) / threads_per_block.x, 
+	                       (dimy + threads_per_block.y - 1) / threads_per_block.y, 
+						   (dimz + threads_per_block.z - 1) / threads_per_block.z);
 
     print_dim3("threads_per_block", threads_per_block);
     print_dim3("blocks_per_grid", blocks_per_grid);
